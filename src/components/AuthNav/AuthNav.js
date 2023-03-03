bcd90182190d0ba7ce97d9089e45b4d81ae781b4
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-03 13:33:16 +0200

    2

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,11 +1,9 @@
 import { StyledNavLink } from './AuthNav.styled';
-import { FcApproval } from 'react-icons/fc';
 
 const AuthNav = () => {
   return (
     <div>
       <StyledNavLink to="/register">Register</StyledNavLink>
-      <FcApproval />
       <StyledNavLink to="/login">Log in</StyledNavLink>
     </div>
   );
